f5e9e171b37dc1b523d7074edae1b8fe7d714c2c
ArteMyasnik <artemyasnik.git@mail.ru>
2025-03-03 23:07:49 +0300

    Новые файлы с результатами. Написал шаблонизаторы, необходимо доделать вход и хранение логина в сессии. Исправил все попытки входа и регистрации.

--- develop/app/server.js
+++ develop/app/server.js
@@ -4,20 +4,30 @@ const app = express();
 const userRouter = require('./controllers/userRoutes');
 const actionRouter = require('./controllers/backend/actionRoutes');
 const PORT = process.env.PORT || 3000;
+const pool = require('./db/db');
 
 app.use(express.static(path.join(__dirname, 'public', 'frontend')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', userRouter);
+
+app.set('view engine', 'ejs'); // Устанавливаем шаблонизатор
+app.set('views', path.join(__dirname, 'public', 'frontend', 'views')); // Указываем папку с шаблонами
+
 app.get("/", (req, res) => {
     res.send('Перейдите по ссылке справа http://localhost:3000/main');
 });
 // Main ------------------------------------------------------------------------------------
-app.get('/main', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/frontend/Pages', 'main_page.html'));
-});
+// app.get('/main', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/frontend/Pages', 'main_page.html'));
+// });
 
+app.get('/main', (req, res) => {
+    const login = req.user ? res.user.login : null; // Логин пользователя
+    console.log(login)
+    res.render('main_page', { login });
+});
 // -----------------------------------------------------------------------------------------
 // Registration and login ------------------------------------------------------------------
 app.get('/registration', (req, res) => {
@@ -41,30 +51,27 @@ app.post('/login', (req, res) => {
 });
 
 // -----------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------
-
-// Профиль изменения возраста и пароля
-app.put('/profile', (req, res) => {
+// Profile changes -------------------------------------------------------------------------
+app.put('/profile/:login/update-info', (req, res) => {
+    const { login } = req.params;
     const { age, sex } = req.body;
-    console.log('Данные входа:', { age, sex });
-    res.status(200).json({ message: 'Изменения выполнены успешно!' });
+    res.status(200).json({ message: 'Возраст и пол обновлены успешно!' });
 });
 
-// -----------------------------------------------------------------------------------------
-// -----------------------------------------------------------------------------------------
-// пароль
-app.put('/profile', (req, res) => {
-    const {oldPassword, newPassword, confirmNewPassword} = req.body;
-    console.log('Данные входа:', { oldPassword, newPassword, confirmNewPassword});
-    res.status(200).json({ message: 'Изменения выполнены успешно!' });
+app.put('/profile/:login/update-password', (req, res) => {
+    const { login } = req.params;
+    const { oldPassword, newPassword, confirmNewPassword } = req.body;
+    res.status(200).json({ message: 'Пароль обновлен успешно!' });
 });
 
-// логин
-app.put('/profile', (req, res) => {
-    const {oldLogin, newLogin} = req.body;
-    console.log('Данные входа:', {oldLogin, newLogin });
-    res.status(200).json({ message: 'Изменения выполнены успешно!' });
+app.put('/profile/:login/update-login', (req, res) => {
+    const { login } = req.params;
+    const { oldLogin, newLogin } = req.body;
+    res.status(200).json({ message: 'Логин обновлен успешно!' });
 });
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
+
 // -----------------------------------------------------------------------------------------
 // Pages HTML professions ------------------------------------------------------------------
 app.get('/businessCompScientist', (req, res) => {
@@ -114,12 +121,53 @@ app.get('/goals', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/frontend/Pages/menu', 'goals.html'));
 });
 
-app.get('/profile', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/frontend/Pages/menu', 'profile.html'));
+// app.get('/profile/:login', (req, res) => {
+//     const { login } = req.params;
+//     res.sendFile(path.join(__dirname, 'public/frontend/Pages/menu', 'profile.html'));
+// });
+
+app.get('/profile/:login', async (req, res) => {
+    const { login } = req.params;
+
+    try {
+        // Получение данных пользователя из базы данных
+        const user = await pool.query('SELECT * FROM users WHERE login = $1', [login]);
+
+        if (user.rows.length === 0) {
+            return res.status(404).send('Пользователь не найден');
+        }
+
+        // Передача данных в шаблон
+        res.render('profile', {
+            login: user.rows[0].login,
+            email: user.rows[0].email,
+            sex: user.rows[0].sex,
+            age: user.rows[0].age
+        });
+    } catch (err) {
+        console.error('Ошибка при получении данных пользователя:', err);
+        res.status(500).send('Ошибка сервера');
+    }
 });
 
 app.get('/developers', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/frontend/Pages/menu', 'developers.html'));
+});
+
+app.get('/pvk', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/frontend/Pages/menu', 'pvk.html'));
+});
+
+app.get('/experts', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/frontend/Pages/menu', 'experts.html'));
+});
+
+app.get('/professions_rating', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/frontend/Pages/menu', 'professions_rating.html'));
+});
+
+app.get('/results', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/frontend/Pages/menu', 'results.html'));
 });
 // -----------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------
