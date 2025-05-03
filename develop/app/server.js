5c29743f3d8548db32647f4766cfb1a318aa650c
ArteMyasnik <artemyasnik.git@mail.ru>
2025-05-03 18:33:40 +0300

    Remake all tests results to store on server. Fix missedSignals problem in db. Rename titles in tests.html

--- develop/app/server.js
+++ develop/app/server.js
@@ -381,9 +381,10 @@ app.get('/test_visual_signal', async (req, res) => {
     const login = req.session.login || null;
     res.render('tests/test_visual_signal', { login });
 });
-app.get('/test_sound', async (req, res) => {
+
+app.get('/test_sound_signal', async (req, res) => {
     const login = req.session.login || null;
-    res.render('tests/test_sound', { login });
+    res.render('tests/test_sound_signal', { login });
 });
 
 app.get('/test_color_signal', async (req, res) => {
@@ -396,10 +397,16 @@ app.get('/test_digital_signal', async (req, res) => {
     res.render('tests/test_digital_signal', { login });
 });
 
+app.get('/test_audio_sum', async (req, res) => {
+    const login = req.session.login || null;
+    res.render('tests/test_audio_sum', { login });
+});
+
 app.get('/test_simple_rdo', async (req, res) => {
     const login = req.session.login || null;
     res.render('tests/test_simple_rdo', { login });
 });
+
 app.get('/test_complex_rdo', async (req, res) => {
     const login = req.session.login || null;
     res.render('tests/test_complex_rdo', { login });
